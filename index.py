0af5786971ab9476e7f3fad0f55f3fd21c9af75a
ketrin clone <ketrinclone@telegmail.com>
2020-09-25 13:04:54 +0700

    first commit

---
+++ index.py
@@ -0,0 +1,36 @@
+print("HELLO HEKTOBERFEST MILLENIALS!!!")
+print("--------------------------------")
+print("Fork it and edit it!")
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
+print("--------------------------------")
+num = int(input("Enter integer value: "))
+if(num == 1):
+    # indented four spaces
+    print ("#hacktoberfest")
+
+if(num == 2):
+    # indented four spaces
+    print ("FORK IT!")
+
+if(num == 3):
+    # indented four spaces
+    print ("PULL IT!")
+
+if(num == 4):
+    # indented four spaces
+    print ("COMMIT NOW!")
+
+if(num == 5):
+    # indented four spaces
+    print ("what r u doing n o w????")
+    s = str(input("ur answer : "))
+    print("you are doing",(s),"everyday???? LOL")
+
+else:
+    crot = 5
+    crott = 3
+    print("HAHAHAAHAH YOU LOVE", crot, crott,"X")
