dbff5115ce50abfe0a24ee68605e1c16a31a69b1
Ferdy Muhammad Iqbal <ferdytata60@gmail.com>
2020-10-01 02:08:50 +0700

    add another search engine

--- hckfest.py
+++ hckfest.py
@@ -1,6 +1,16 @@
-class Hckfest:
-    import webbrowser
-    link = "https://github.com/kakeyrin/"
-    print("\nWelcome to hektoberfezzz maderfaderr\nGOBRRRRRRRRRRR")
-    webbrowser.open(link)
-    exit()
+def Hckfest():
+    i = 1
+    while i == 1:
+        rows1 = 5
+        for crit in range(1, rows1+1):
+            for column in range(1, crit+1):
+                print(column, end=" ")
+            print(" ")
+        print("1 2 3 4 5 6")
+        rows3 = 5
+        c = rows3
+        for crit2 in range(rows3, 0, -1):
+            for column2 in range(0, crit2):
+                print(c, end=' ')
+            print("\r")
+        continue
--- index.py
+++ index.py
@@ -2,8 +2,7 @@ from account import *
 def displayMenu():
     print("welcome Hacktoberfest! :D\n")
     print("Sign in & Sign up\n")
-    print("Are you already sign up here?")
-    print("y = yes\nn = no\nq = quit\n")
+    print("- type y to Login\n- type n to Sign up\n- type q to Quit\n")
     status = input("Answer : ")
     if status == "y":
         oldUser()
@@ -15,17 +14,21 @@ def displayMenu():
         wrongAns()
  
 def newUser():
-    createLogin = input("\nCreate username : ")
+    print("\n-----Sign up-----")
+    createLogin = input("Create username : ")
  
     if createLogin in users:
-        print("\nLogin name already exist!")
+        print("\nusername already exist!")
+        newUser()
     else:
         createPassw = input("Create password : ")
         users[createLogin] = createPassw
-        print("\nUser created! Please Log in your account.")
+        print("\nUser created! Please Log in to your account.")
+        oldUser()
  
 def oldUser():
-    login = input("\nEnter username : ")
+    print("\n-----Login-----")
+    login = input("Enter username : ")
     passw = input("Enter password : ")
  
     if login in users and users[login] == passw:
@@ -33,39 +36,108 @@ def oldUser():
         mainmenu()
 
     else:
-        print("\nUser doesn't exist or wrong password!")
-        oldUser()
+        print("\nUsername doesn't exist or wrong password!")
+        create_account = input("want to create your account?\n- y for yes\n- n for no\nAnswer : ")
+        if create_account == "y":
+            newUser()
+        elif create_account == "n":
+            oldUser()
+        else:
+            displayMenu()
 
 def mainmenu():
-    z = input("\nChoose what you wanna do!\nHektober unknown code or Google Search?\nfor Hektober type hek\nfor Google Search type google\nAnswer : ")
-    if z == "hek":
+    print("\n------ MAIN MENU ------\n")
+    choose_mainmenu = input("Choose what you wanna do!\nSee the mulitiple choice above!\n- for Hektobercode type hek\n- for Google Search type google\n- for Yandex Search type yandex\n- for exit type exit\nAnswer : ")
+    if choose_mainmenu == "hek":
         hekfestcode()
-    elif z == "google":
+    elif choose_mainmenu == "google":
         google()
+    elif choose_mainmenu == "yandex":
+        yandex()
+    elif choose_mainmenu == "exit":
+        exit()
     else:
         wrongAns()
 
 def google():
-    i = 1
-    while i == 1:
+    print("\n--- Google Searcher ---")
+    print("what do you want to search?")
+    print("1 = web search\n2 = images search\n3 = videos search")
+    choose_pick_google = input("Answer : ")
+    if choose_pick_google == "1":
         import webbrowser
-        print("\n--- Google Searcher ---")
-        go = input("\nWhat do you want to search?\nAnswer : ")
-        link = "https://www.google.com/search?q="
-        webbrowser.open(link + go)
-        q = input("\n\nWant to exit? go to main menu? or repeat searching?\ntype q to quit\ntype d to go to main menu\ntype r to repeat search\nAnswer : ")
-        if q == "q":
-            exit()
-        elif q == "d":
-            oldUser()
-        elif q == "r":   
-            continue
-        else:
-            wrongAns()
+        input_web_google = input("\nWhat do you want to search?\nAnswer : ")
+        link_web_google = "https://www.google.com/search?q="
+        webbrowser.open(link_web_google + input_web_google)
+        retryGoogle()
+    elif choose_pick_google == "2":
+        import webbrowser
+        input_images_google = input("\nWhat do you want to search?\nAnswer : ")
+        link_images_google = "https://www.google.com/search?q="
+        encrypt_link_google_images = "&sxsrf=ALeKk00fjz7RfbMwi2wFrVB_ng7z11nVNQ:1601287281064&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiKv8j8y4vsAhXc7XMBHUn8BU4Q_AUoA3oECB4QBQ&biw=1920&bih=969"
+        webbrowser.open(link_images_google + input_images_google + encrypt_link_google_images)
+        retryGoogle()
+    elif choose_pick_google == "3":
+        import webbrowser
+        input_videos_google = input("\nWhat do you want to search?\nAnswer : ")
+        link_videos_google = "https://www.google.com/search?q="
+        encrypt_link_google_videos = "&source=lmns&tbm=vid&bih=969&biw=1920&hl=id&sa=X&ved=2ahUKEwj2vM6KzIvsAhW5y3MBHQ5wDr0Q_AUoAnoECAEQAg"
+        webbrowser.open(link_videos_google + input_videos_google + encrypt_link_google_videos)
+        retryGoogle()
+    else:
+        wrongAns()
+
+def retryGoogle():
+    retry_google = input("\nWant to exit? go to main menu? or repeat searching?\ntype q to quit\ntype d to go to main menu\ntype r to repeat search\nAnswer : ")
+    if retry_google == "q":
+        exit()
+    elif retry_google == "d":
+        oldUser()
+    elif retry_google == "r":   
+        google()
+    else:
+        wrongAns()
+
+def yandex():
+    print("\n--- Yandex Searcher ---")
+    print("what do you want to search?")
+    print("1 = web search\n2 = images search\n3 = videos search")
+    choose_pick_yandex = input("Answer : ")
+    if choose_pick_yandex == "1":
+        import webbrowser
+        input_web_yandex = input("\nWhat do you want to search?\nAnswer : ")
+        link_web_yandex = "https://yandex.com/search/?text="
+        webbrowser.open(link_web_yandex + input_web_yandex)
+        retryYandex()
+    elif choose_pick_yandex == "2":
+        import webbrowser
+        input_images_yandex = input("\nWhat do you want to search?\nAnswer : ")
+        link_images_yandex = "https://yandex.com/images/search?text="
+        webbrowser.open(link_images_yandex + input_images_yandex)
+        retryYandex()
+    elif choose_pick_yandex == "3":
+        import webbrowser
+        input_videos_yandex = input("\nWhat do you want to search?\nAnswer : ")
+        link_videos_yandex = "https://yandex.com/video/search?text="
+        webbrowser.open(link_videos_yandex + input_videos_yandex)
+        retryYandex()
+    else:
+        wrongAns()
+
+def retryYandex():
+    retry_yandex = input("\nWant to exit? go to main menu? or repeat searching?\ntype q to quit\ntype d to go to main menu\ntype r to repeat search\nAnswer : ")
+    if retry_yandex == "q":
+        exit()
+    elif retry_yandex == "d":
+        oldUser()
+    elif retry_yandex == "r":   
+        yandex()
+    else:
+        wrongAns()
 
 def hekfestcode():
     print("\nFork it and edit it!")
-    print("\n--------------------------------")
+    print("--------------------------------")
     print("1")
     print("2")
     print("3")
